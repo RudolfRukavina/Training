e5c3611b36fddb20ffa1dc372cbc22b683343e5d
RudolfRukavina <rukavina.rudi@gmail.com>
2023-12-07 17:51:21 +0100

    Login animation added & code structure

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,60 @@
 <template>
-  <div>
-    <button @click="useUserStore().handleSignInGoogle" v-if="!useUserStore().userData">Google Sign In</button>
-    <p v-if="useUserStore().user">Hello {{ useUserStore().userData.displayName || useUserStore().userData.email }}</p>
-    <button @click="useUserStore().handleLogOut" v-if="useUserStore().userData">Log out</button>
-    <div v-if="useUserStore().userData">
-      <p>User Data:</p>
-      <ul v-if="useUserStore().userData">
-        <li>Username: {{ useUserStore().userData.username }}</li>
-        <li>Email: {{ useUserStore().userData.email }}</li>
-        <!-- Add other user-specific data fields here -->
-      </ul>
-      <button @click="useUserStore().updateUserData">Update User Data</button>
-      {{ useUserStore().userData }}
+  <div class="bg-white">
+    <div class="relative isolate px-6 lg:px-8">
+      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
+      <div class="mx-auto max-w-2xl py-48">
+        <div class="text-center">
+          <h1 class="text-8xl font-bold tracking-tight text-gray-900">Fittter</h1>
+          <h2 class="text-2xl font-bold tracking-tight text-gray-900">Strength Training Web Application</h2>
+          <p class="mt-2 text-lg leading-8 text-gray-600">It is free and always will be.</p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
 
-      <p class='my-2 text-3xl text-center' v-if='useUserStore().userData'
-        @click="useUserStore().userData.hypertrophy[index] = 1"
-        v-for='(item, index) in useUserStore().userData.hypertrophy'>{{ item }}
-      </p>
+            <button class="gsi-material-button" @click="useUserStore().handleSignInGoogle">
+              <div class="gsi-material-button-state"></div>
+              <div class="gsi-material-button-content-wrapper">
+                <div class="gsi-material-button-icon">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block">
+                    <path fill="#EA4335"
+                      d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z">
+                    </path>
+                    <path fill="#4285F4"
+                      d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z">
+                    </path>
+                    <path fill="#FBBC05"
+                      d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z">
+                    </path>
+                    <path fill="#34A853"
+                      d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z">
+                    </path>
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                  </svg>
+                </div>
+                <span class="gsi-material-button-contents">Sign in</span>
+                <span style="display: none">Sign in with Google</span>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="absolute inset-x-0 top-[40%] -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
+      </div>
     </div>
   </div>
 </template>
-
 <script setup>
   import { useUserStore } from "../stores/user";
+  import { ref, onBeforeMount } from "vue"
+
+  onBeforeMount(() => {
+    useUserStore().getUser()
+  })
+
 </script>
